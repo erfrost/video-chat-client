2c966660d6310b2ac7d3c1f65f6c2947dcd3a837
Дмитрий <dmitriy@MacBook-Air-Dmitrij.local>
2024-07-22 19:23:03 +0500

    update domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost, http://147.45.246.145:8000"],
+    domains: ["localhost", "147.45.246.145"],
   },
 };
 
